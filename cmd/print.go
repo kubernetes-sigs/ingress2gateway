ab7287dad149d012482f71be0c8d2edfcdb16cf5
Kobi Levi <kobilevi503@gmail.com>
2023-06-21 07:51:43 +0300

    Add comments and licensing information

--- cmd/print.go
+++ cmd/print.go
@@ -1,25 +1,39 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package cmd
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/printers"
-	"strings"
 )
 
 var (
 	output = "yaml"
 )
 
-// printCmd represents the print command
+// printCmd represents the print command. It prints ingress-converted
+// HTTPRoutes and Gateways.
 var printCmd = &cobra.Command{
 	Use:   "print",
-	Short: "A brief description of your command",
+	Short: "Prints ingress-converted HTTPRoutes and Gateways",
 	Run: func(cmd *cobra.Command, args []string) {
 		resourcePrinter := getResourcePrinter()
 		i2gw.Run(resourcePrinter)
