44931f48fe04f877d1c85836cb772971e1052ace
Nathan Luong <luongtrinhan@gmail.com>
2023-07-25 02:34:53 +0100

    Refactor print.go to have the printRunner object

--- cmd/print.go
+++ cmd/print.go
@@ -27,10 +27,10 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var (
+type PrintRunner struct {
 	// outputFormat contains currently set output format. Value assigned via --output/-o flag.
 	// Defaults to YAML.
-	outputFormat = "yaml"
+	outputFormat string
 
 	// The path to the input yaml config file. Value assigned via --input_file flag
 	inputFile string
@@ -43,59 +43,112 @@ var (
 	// allNamespaces indicates whether all namespaces should be used. Value assigned via
 	// --all-namespaces/-A flag.
 	allNamespaces bool
-)
 
-// printCmd represents the print command. It prints HTTPRoutes and Gateways
-// generated from Ingress resources.
-var printCmd = &cobra.Command{
-	Use:   "print",
-	Short: "Prints HTTPRoutes and Gateways generated from Ingress resources",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		resourcePrinter, err := getResourcePrinter(outputFormat)
-		if err != nil {
-			return err
-		}
-		namespaceFilter, err := getNamespaceFilter(namespace, allNamespaces)
-		if err != nil && inputFile == "" {
+	// resourcePrinter determines how resource objects are printed out
+	resourcePrinter printers.ResourcePrinter
+
+	// Only resources that matches this filter will be processed.
+	namespaceFilter string
+}
+
+func NewDefaultPrintRunner() *PrintRunner {
+	pr := &PrintRunner{}
+	pr.outputFormat = "yaml"
+	return pr
+}
+
+func (pr *PrintRunner) PrintGatewaysAndHttpRoutes(cmd *cobra.Command, args []string) error {
+	i2gw.Run(pr.resourcePrinter, pr.namespaceFilter, pr.inputFile)
+	return nil
+}
+
+// InitializeResourcePrinter assign a specific type of printers.ResourcePrinter
+// based on the outputFormat of the printRunner struct.
+func (pr *PrintRunner) InitializeResourcePrinter() error {
+	switch pr.outputFormat {
+	case "yaml", "":
+		pr.resourcePrinter = &printers.YAMLPrinter{}
+		return nil
+	case "json":
+		pr.resourcePrinter = &printers.JSONPrinter{}
+		return nil
+	default:
+		return fmt.Errorf("%s is not a supported output format", pr.outputFormat)
+	}
+
+}
+
+func (pr *PrintRunner) InitializeNamespaceFilter() error {
+	// When we should use all namespaces, empty string is used as the filter.
+	if pr.allNamespaces {
+		pr.namespaceFilter = ""
+		return nil
+	}
+
+	if pr.namespace == "" {
+		ns, err := getNamespaceInCurrentContext()
+		if err != nil && pr.inputFile == "" {
 			// When asked to read from the cluster, but getting the current namespace
 			// failed for whatever reason - do not process the request.
 			return err
 		}
 		// If err is nil we got the right filtered namespace.
 		// If the input file is specified, and we failed to get the namespace, use all namespaces.
-		i2gw.Run(resourcePrinter, namespaceFilter, inputFile)
+		pr.namespaceFilter = ns
 		return nil
-	},
+	}
+
+	pr.namespaceFilter = pr.namespace
+	return nil
 }
 
-// getResourcePrinter returns a specific type of printers.ResourcePrinter
-// based on the provided outputFormat.
-func getResourcePrinter(outputFormat string) (printers.ResourcePrinter, error) {
-	switch outputFormat {
-	case "yaml", "":
-		return &printers.YAMLPrinter{}, nil
-	case "json":
-		return &printers.JSONPrinter{}, nil
-	default:
-		return nil, fmt.Errorf("%s is not a supported output format", outputFormat)
+func newPrintCommand() *cobra.Command {
+	pr := NewDefaultPrintRunner()
+	var printFlags genericclioptions.JSONYamlPrintFlags
+	allowedFormats := printFlags.AllowedFormats()
+
+	// printCmd represents the print command. It prints HTTPRoutes and Gateways
+	// generated from Ingress resources.
+	var cmd = &cobra.Command{
+		Use:   "print",
+		Short: "Prints HTTPRoutes and Gateways generated from Ingress resources",
+		RunE:  pr.PrintGatewaysAndHttpRoutes,
 	}
+
+	cmd.RunE = pr.PrintGatewaysAndHttpRoutes
+
+	cmd.Flags().StringVarP(&pr.outputFormat, "output", "o", "yaml",
+		fmt.Sprintf(`Output format. One of: (%s)`, strings.Join(allowedFormats, ", ")))
+
+	cmd.Flags().StringVar(&pr.inputFile, "input_file", "",
+		`Path to the manifest file. When set, the tool will read ingresses from the file instead of reading from the cluster. Supported files are yaml and json`)
+
+	cmd.Flags().StringVarP(&pr.namespace, "namespace", "n", "",
+		`If present, the namespace scope for this CLI request`)
+
+	cmd.Flags().BoolVarP(&pr.allNamespaces, "all-namespaces", "A", false,
+		`If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even
+if specified with --namespace.`)
+
+	cmd.MarkFlagsMutuallyExclusive("namespace", "all-namespaces")
+	return cmd
 }
 
 // getNamespaceFilter returns a namespace filter, taking into consideration whether a specific
 // namespace is requested, or all of them are.
-func getNamespaceFilter(requestedNamespace string, useAllNamespaces bool) (string, error) {
+func getNamespaceFilter(pr *PrintRunner) (string, error) {
 
 	// When we should use all namespaces, return an empty string.
 	// This is the first condition since it should override the requestedNamespace,
 	// if specified.
-	if useAllNamespaces {
+	if pr.allNamespaces {
 		return "", nil
 	}
 
-	if requestedNamespace == "" {
+	if pr.namespace == "" {
 		return getNamespaceInCurrentContext()
 	}
-	return requestedNamespace, nil
+	return pr.namespace, nil
 }
 
 // getNamespaceInCurrentContext returns the namespace in the current active context of the user.
@@ -109,23 +162,6 @@ func getNamespaceInCurrentContext() (string, error) {
 }
 
 func init() {
-	var printFlags genericclioptions.JSONYamlPrintFlags
-	allowedFormats := printFlags.AllowedFormats()
-
-	printCmd.Flags().StringVarP(&outputFormat, "output", "o", "yaml",
-		fmt.Sprintf(`Output format. One of: (%s)`, strings.Join(allowedFormats, ", ")))
-
-	printCmd.Flags().StringVar(&inputFile, "input_file", "",
-		`Path to the manifest file. When set, the tool will read ingresses from the file instead of reading from the cluster. Supported files are yaml and json`)
-
-	printCmd.Flags().StringVarP(&namespace, "namespace", "n", "",
-		`If present, the namespace scope for this CLI request`)
-
-	printCmd.Flags().BoolVarP(&allNamespaces, "all-namespaces", "A", false,
-		`If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even
-if specified with --namespace.`)
-
-	printCmd.MarkFlagsMutuallyExclusive("namespace", "all-namespaces")
-
+	printCmd := newPrintCommand()
 	rootCmd.AddCommand(printCmd)
 }
