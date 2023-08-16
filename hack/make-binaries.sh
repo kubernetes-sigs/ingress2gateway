c5384a70ce6e43d4ce43a0397ed0f18520f5e893
Lior Lieberman <liorlieberman@google.com>
2023-08-16 12:31:42 +0000

    fixes

--- hack/make-binaries.sh
+++ hack/make-binaries.sh
@@ -39,5 +39,5 @@ echo >&2 "(Stamping with git tag=${git_tag} rev=${git_rev})"
 env CGO_ENABLED=0 gox -osarch="${OSARCH:-$supported_platforms}" \
   -tags netgo \
   -mod readonly \
-  -output="out/bin/krew-{{.OS}}_{{.Arch}}" \
-  ./cmd/krew/...
+  -output="out/bin/ingress2gateway-{{.OS}}_{{.Arch}}" \
+  ./...
