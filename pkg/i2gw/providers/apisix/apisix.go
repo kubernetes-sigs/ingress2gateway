e314aba7ed41585d906eb28f67b278c0cacbfeb8
Navendu Pottekkat <navendu@apache.org>
2023-12-15 15:01:54 +0530

    add initial support for Apache APISIX provider
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

---
+++ pkg/i2gw/providers/apisix/apisix.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apisix
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// The Name of the provider.
+const Name = "apisix"
+const ApisixIngressClass = "apisix"
+
+func init() {
+	i2gw.ProviderConstructorByName[Name] = NewProvider
+}
+
+// Provider implements the i2gw.Provider interface.
+type Provider struct {
+	storage        *storage
+	resourceReader *resourceReader
+	converter      *converter
+}
+
+// NewProvider constructs and returns the apisix implementation of i2gw.Provider.
+func NewProvider(conf *i2gw.ProviderConf) i2gw.Provider {
+	return &Provider{
+		storage:        newResourcesStorage(),
+		resourceReader: newResourceReader(conf),
+		converter:      newConverter(),
+	}
+}
+
+// ToGatewayAPI converts the received i2gw.InputResources to i2gw.GatewayResources
+// including the apisix specific features.
+func (p *Provider) ToGatewayAPI(_ i2gw.InputResources) (i2gw.GatewayResources, field.ErrorList) {
+	return p.converter.convert(p.storage)
+}
+
+func (p *Provider) ReadResourcesFromCluster(ctx context.Context) error {
+	storage, err := p.resourceReader.readResourcesFromCluster(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to read resources from cluster: %w", err)
+	}
+
+	p.storage = storage
+	return nil
+}
+
+func (p *Provider) ReadResourcesFromFile(_ context.Context, filename string) error {
+	storage, err := p.resourceReader.readResourcesFromFile(filename)
+	if err != nil {
+		return fmt.Errorf("failed to read resources from file: %w", err)
+	}
+
+	p.storage = storage
+	return nil
+}
---
+++ pkg/i2gw/providers/apisix/resource_reader.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apisix
+
+import (
+	"context"
+
+	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
+	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/providers/common"
+)
+
+// converter implements the i2gw.CustomResourceReader interface.
+type resourceReader struct {
+	conf *i2gw.ProviderConf
+}
+
+// newResourceReader returns a resourceReader instance.
+func newResourceReader(conf *i2gw.ProviderConf) *resourceReader {
+	return &resourceReader{
+		conf: conf,
+	}
+}
+
+func (r *resourceReader) readResourcesFromCluster(ctx context.Context) (*storage, error) {
+	// read apisix related resources from cluster.
+	storage := newResourcesStorage()
+
+	ingresses, err := common.ReadIngressesFromCluster(ctx, r.conf.Client, ApisixIngressClass)
+	if err != nil {
+		return nil, err
+	}
+	storage.Ingresses = ingresses
+	return storage, nil
+}
+
+func (r *resourceReader) readResourcesFromFile(filename string) (*storage, error) {
+	// read apisix related resources from file.
+	storage := newResourcesStorage()
+
+	ingresses, err := common.ReadIngressesFromFile(filename, r.conf.Namespace, ApisixIngressClass)
+	if err != nil {
+		return nil, err
+	}
+	storage.Ingresses = ingresses
+	return storage, nil
+}
