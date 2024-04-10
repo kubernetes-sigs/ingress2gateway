f1125971f301a771602adcccc9c2814005b9b093
Lior Lieberman <liorlieberman@google.com>
2024-04-10 07:13:04 +0000

    fail soft when crds are not installed

--- pkg/i2gw/providers/kong/resource_reader.go
+++ pkg/i2gw/providers/kong/resource_reader.go
@@ -24,6 +24,8 @@ import (
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kongv1beta1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1beta1"
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
@@ -92,7 +94,11 @@ func (r *resourceReader) readTCPIngressesFromCluster(ctx context.Context) ([]kon
 
 	err := r.conf.Client.List(ctx, tcpIngressList)
 	if err != nil {
-		return nil, fmt.Errorf("failed to list Kong TCPIngresses: %w", err)
+		if client.IgnoreNotFound(err) == nil {
+			klog.Warningf("couldn't find %s CRD, it is likely not installed in the cluster", tcpIngressGVK.GroupKind().String())
+			return []kongv1beta1.TCPIngress{}, nil
+		}
+		return nil, fmt.Errorf("failed to list %s: %w", tcpIngressGVK.GroupKind().String(), err)
 	}
 
 	tcpIngresses := []kongv1beta1.TCPIngress{}
