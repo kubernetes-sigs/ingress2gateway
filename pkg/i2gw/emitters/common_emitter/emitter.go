bb65c6452b27fd845df41c07566d92dbc1ca29c2
Steven Jin Xuan <sjinxuan@microsoft.com>
2025-12-30 11:15:19 -0500

    non-regex rewrites

--- pkg/i2gw/emitters/common_emitter/emitter.go
+++ pkg/i2gw/emitters/common_emitter/emitter.go
@@ -19,6 +19,7 @@ package common_emitter
 import (
 	emitterir "github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/emitter_intermediate"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 type Emitter struct{}
@@ -27,9 +28,33 @@ func NewEmitter() *Emitter {
 	return &Emitter{}
 }
 
+func applyPathRewrites(ir *emitterir.EmitterIR) {
+	for key, routeCtx := range ir.HTTPRoutes {
+		for ruleIdx, rewrite := range routeCtx.PathRewriteByRuleIdx {
+			if rewrite == nil {
+				continue
+			}
+			fullPath := rewrite.ReplaceFullPath
+			routeCtx.Spec.Rules[ruleIdx].Filters = append(routeCtx.Spec.Rules[ruleIdx].Filters, gatewayv1.HTTPRouteFilter{
+				Type: gatewayv1.HTTPRouteFilterURLRewrite,
+				URLRewrite: &gatewayv1.HTTPURLRewriteFilter{
+					Path: &gatewayv1.HTTPPathModifier{
+						Type:            gatewayv1.FullPathHTTPPathModifier,
+						ReplaceFullPath: &fullPath,
+					},
+				},
+			})
+			routeCtx.PathRewriteByRuleIdx[ruleIdx] = nil
+		}
+
+		ir.HTTPRoutes[key] = routeCtx
+	}
+}
+
 // Emit processes the IR to apply common logic (like deduplication) and returns the modified IR.
 // This ALWAYS runs after providers and before provider-specific emitters.
 // TODO: Implement common logic such as filtering by maturity status and/or individual features.
 func (e *Emitter) Emit(ir emitterir.EmitterIR) (emitterir.EmitterIR, field.ErrorList) {
+	applyPathRewrites(&ir)
 	return ir, nil
 }
