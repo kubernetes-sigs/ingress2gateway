4f693fac30b2fca168fa6bba83b80492ae094971
Johanan Liebermann <jliebermann@microsoft.com>
2026-01-08 12:30:47 +0200

    Add main test logic
    Signed-off-by: Johanan Liebermann <jliebermann@microsoft.com>

--- e2e/util.go
+++ e2e/util.go
@@ -17,8 +17,29 @@ limitations under the License.
 package e2e
 
 import (
+	"crypto/rand"
+
 	"sigs.k8s.io/yaml"
 )
+
+// Generates a cryptographically random alphanumeric string of length n. Uses crypto/rand to ensure
+// uniqueness even when called from parallel tests.
+func randString(n int) (string, error) {
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+
+	// Populate b with random alphanumeric characters by indexing chars with the remainder of
+	// (b[i] / len(chars)). This ensures we always select a valid element from chars. The byte()
+	// conversion is required because the % operator expects two operands of the same type.
+	for i := range b {
+		b[i] = chars[b[i]%byte(len(chars))]
+	}
+
+	return string(b), nil
+}
 
 // Converts a k8s object to a YAML string.
 func toYAML(obj interface{}) (string, error) {
