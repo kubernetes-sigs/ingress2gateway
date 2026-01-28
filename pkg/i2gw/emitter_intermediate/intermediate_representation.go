7f3f1f8cb2feb31cf740c1ab1bf853cddd100122
Chakri Dudyala <dcvr.1997@gmail.com>
2026-01-28 08:17:58 +0530

    Update pkg/i2gw/emitter_intermediate/intermediate_representation.go
    Co-authored-by: Steven Jin <stevenjin8@gmail.com>

--- pkg/i2gw/emitter_intermediate/intermediate_representation.go
+++ pkg/i2gw/emitter_intermediate/intermediate_representation.go
@@ -17,12 +17,12 @@ limitations under the License.
 package emitterir
 
 import (
-	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/emitter_intermediate/gce"
-	"k8s.io/apimachinery/pkg/api/resource"
-	"k8s.io/apimachinery/pkg/types"
-	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
-	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
-	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+        "github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/emitter_intermediate/gce"
+        "k8s.io/apimachinery/pkg/api/resource"
+        "k8s.io/apimachinery/pkg/types"
+        gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+        gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+        gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
 // EmitterIR holds specifications of Gateway Objects for supporting Ingress extensions,
@@ -30,80 +30,78 @@ import (
 // features. An EmitterIR field can be mapped to core Gateway-API fields,
 // or provider-specific Gateway extensions.
 type EmitterIR struct {
-	Gateways   map[types.NamespacedName]GatewayContext
-	HTTPRoutes map[types.NamespacedName]HTTPRouteContext
+        Gateways   map[types.NamespacedName]GatewayContext
+        HTTPRoutes map[types.NamespacedName]HTTPRouteContext
 
-	GatewayClasses map[types.NamespacedName]GatewayClassContext
-	TLSRoutes      map[types.NamespacedName]TLSRouteContext
-	TCPRoutes      map[types.NamespacedName]TCPRouteContext
-	UDPRoutes      map[types.NamespacedName]UDPRouteContext
-	GRPCRoutes     map[types.NamespacedName]GRPCRouteContext
+        GatewayClasses map[types.NamespacedName]GatewayClassContext
+        TLSRoutes      map[types.NamespacedName]TLSRouteContext
+        TCPRoutes      map[types.NamespacedName]TCPRouteContext
+        UDPRoutes      map[types.NamespacedName]UDPRouteContext
+        GRPCRoutes     map[types.NamespacedName]GRPCRouteContext
 
-	BackendTLSPolicies map[types.NamespacedName]BackendTLSPolicyContext
-	ReferenceGrants    map[types.NamespacedName]ReferenceGrantContext
+        BackendTLSPolicies map[types.NamespacedName]BackendTLSPolicyContext
+        ReferenceGrants    map[types.NamespacedName]ReferenceGrantContext
 
-	GceServices map[types.NamespacedName]gce.ServiceIR
+        GceServices map[types.NamespacedName]gce.ServiceIR
 }
 
 type GatewayContext struct {
-	gatewayv1.Gateway
-	// Emitter IR should be provider/emitter neutral,
-	// But we have GCE for backcompatibility.
-	Gce *gce.GatewayIR
+        gatewayv1.Gateway
+        // Emitter IR should be provider/emitter neutral,
+        // But we have GCE for backcompatibility.
+        Gce *gce.GatewayIR
 }
 
 type HTTPRouteContext struct {
-	gatewayv1.HTTPRoute
-	// PathRewriteByRuleIdx maps HTTPRoute rule indices to path rewrite intent.
-	// This is provider-neutral and applied by the common emitter.
-	PathRewriteByRuleIdx map[int]*PathRewrite
+        gatewayv1.HTTPRoute
+        // PathRewriteByRuleIdx maps HTTPRoute rule indices to path rewrite intent.
+        // This is provider-neutral and applied by the common emitter.
+        PathRewriteByRuleIdx map[int]*PathRewrite
 
-	// BodySizeByRuleIdx maps HTTPRoute rule indices to body size intent.
-	// This is provider-neutral and applied by each custom emitter.
-	BodySizeByRuleIdx map[int]*BodySize
-
-	Gce *gce.HTTPRouteIR
+        // BodySizeByRuleIdx maps HTTPRoute rule indices to body size intent.
+        // This is provider-neutral and applied by each custom emitter.
+        BodySizeByRuleIdx map[int]*BodySize
 }
 
 // PathRewrite represents provider-neutral path rewrite intent.
 // For now it only supports full-path replacement; more fields may be added later.
 type PathRewrite struct {
-	ReplaceFullPath string
-	// Headers to add on path rewrite.
-	Headers map[string]string
-	Regex   bool
+        ReplaceFullPath string
+        // Headers to add on path rewrite.
+        Headers map[string]string
+        Regex   bool
 }
 
 // BodySize represents provider-neutral body size intent.
 type BodySize struct {
-	BufferSize *resource.Quantity
-	MaxSize    *resource.Quantity
+        BufferSize *resource.Quantity
+        MaxSize    *resource.Quantity
 }
 
 type GatewayClassContext struct {
-	gatewayv1.GatewayClass
+        gatewayv1.GatewayClass
 }
 
 type TLSRouteContext struct {
-	gatewayv1alpha2.TLSRoute
+        gatewayv1alpha2.TLSRoute
 }
 
 type TCPRouteContext struct {
-	gatewayv1alpha2.TCPRoute
+        gatewayv1alpha2.TCPRoute
 }
 
 type UDPRouteContext struct {
-	gatewayv1alpha2.UDPRoute
+        gatewayv1alpha2.UDPRoute
 }
 
 type GRPCRouteContext struct {
-	gatewayv1.GRPCRoute
+        gatewayv1.GRPCRoute
 }
 
 type BackendTLSPolicyContext struct {
-	gatewayv1.BackendTLSPolicy
+        gatewayv1.BackendTLSPolicy
 }
 
 type ReferenceGrantContext struct {
-	gatewayv1beta1.ReferenceGrant
+        gatewayv1beta1.ReferenceGrant
 }
