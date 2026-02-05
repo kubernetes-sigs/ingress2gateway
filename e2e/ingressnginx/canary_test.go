4bc978e1112a3d956dedbf0d349c059ac28b3fcc
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-02-05 13:00:56 -0500

    random canary host

--- e2e/ingressnginx/canary_test.go
+++ e2e/ingressnginx/canary_test.go
@@ -17,12 +17,14 @@ limitations under the License.
 package ingressnginx
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/kubernetes-sigs/ingress2gateway/e2e"
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/providers/ingressnginx"
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/providers/istio"
+	"github.com/stretchr/testify/require"
 	networkingv1 "k8s.io/api/networking/v1"
 )
 
@@ -31,6 +33,9 @@ func TestCanary(t *testing.T) {
 	t.Run("to Istio", func(t *testing.T) {
 		t.Parallel()
 		t.Run("base canary", func(t *testing.T) {
+			suffix, err := e2e.RandString(6)
+			require.NoError(t, err)
+			host := fmt.Sprintf("canary-%s.com", suffix)
 			e2e.RunTestCase(t, &e2e.TestCase{
 				GatewayImplementation: istio.ProviderName,
 				Providers:             []string{ingressnginx.Name},
@@ -42,12 +47,12 @@ func TestCanary(t *testing.T) {
 				Ingresses: []*networkingv1.Ingress{
 					e2e.BasicIngress().
 						WithName("foo1").
-						WithHost("canary.com").
+						WithHost(host).
 						WithIngressClass(ingressnginx.NginxIngressClass).
 						Build(),
 					e2e.BasicIngress().
 						WithName("foo2").
-						WithHost("canary.com").
+						WithHost(host).
 						WithIngressClass(ingressnginx.NginxIngressClass).
 						WithAnnotation("nginx.ingress.kubernetes.io/canary", "true").
 						WithAnnotation("nginx.ingress.kubernetes.io/canary-weight", "20").
@@ -57,7 +62,7 @@ func TestCanary(t *testing.T) {
 				Verifiers: map[string][]e2e.Verifier{
 					"foo1": {
 						&e2e.CanaryVerifier{
-							Verifier:     &e2e.HttpGetVerifier{Host: "canary.com", Path: "/hostname", BodyRegex: regexp.MustCompile("^dummy-app2")},
+							Verifier:     &e2e.HttpGetVerifier{Host: host, Path: "/hostname", BodyRegex: regexp.MustCompile("^dummy-app2")},
 							Runs:         200,
 							MinSuccesses: 0.7,
 							MaxSuccesses: 0.9,
