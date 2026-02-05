10966aa5a6deb84d87938023a90a4ab7cf45fc2d
Chakri Dudyala <chakravardhan@google.com>
2026-02-05 10:07:43 +0000

    style: gofmt changes

--- pkg/i2gw/emitters/gce/gce_test.go
+++ pkg/i2gw/emitters/gce/gce_test.go
@@ -614,7 +614,6 @@ func getTestHealthCheckPolicyUnstrctured(serviceNamespace, serviceName, protocol
 	return *hcPolicyUnstructured
 }
 
-
 func Test_GatewayClassNameOverride(t *testing.T) {
 	testCases := []struct {
 		name              string
