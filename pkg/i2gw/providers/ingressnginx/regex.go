6268876f8290ed2877cde858f9457550011ba9f7
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-02-17 11:13:27 -0500

    host level regex, and prefix regex

--- pkg/i2gw/providers/ingressnginx/regex.go
+++ pkg/i2gw/providers/ingressnginx/regex.go
@@ -23,50 +23,59 @@ import (
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/utils/ptr"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 // regexFeature converts the "nginx.ingress.kubernetes.io/use-regex" annotation
 // to Gateway API HTTPRoute RegularExpression path match.
-func regexFeature(_ []networkingv1.Ingress, _ map[types.NamespacedName]map[string]int32, ir *providerir.ProviderIR) field.ErrorList {
+func regexFeature(ingresses []networkingv1.Ingress, _ map[types.NamespacedName]map[string]int32, ir *providerir.ProviderIR) field.ErrorList {
 	var errs field.ErrorList
 
+	hostsWithRegex := make(map[string]struct{})
+
+	for _, ingress := range ingresses {
+		// TODO if there is a rewrite-target annotation, the path is treated as a regex even if use-regex is not set to true. We should also check that.
+		val, ok := ingress.Annotations[UseRegexAnnotation]
+		if !ok {
+			continue
+		}
+		useRegex, _ := strconv.ParseBool(val)
+		if !useRegex {
+			continue
+		}
+		val = ingress.Annotations[CanaryAnnotation]
+		isCanary, _ := strconv.ParseBool(val)
+		if isCanary {
+			continue
+		}
+
+		for _, rule := range ingress.Spec.Rules {
+			if rule.Host != "" {
+				hostsWithRegex[rule.Host] = struct{}{}
+			}
+		}
+	}
+
 	for _, httpRouteCtx := range ir.HTTPRoutes {
-		for ruleIdx, rule := range httpRouteCtx.Spec.Rules {
-			if ruleIdx >= len(httpRouteCtx.RuleBackendSources) {
-				continue
+		hasRegex := false
+		for _, host := range httpRouteCtx.Spec.Hostnames {
+			if _, found := hostsWithRegex[string(host)]; found {
+				hasRegex = true
+				break
 			}
-			sources := httpRouteCtx.RuleBackendSources[ruleIdx]
-			if len(sources) == 0 {
-				continue
-			}
+		}
+		if !hasRegex {
+			continue
+		}
 
-			// Check if the source ingress has the regex annotation enabled.
-			ingress := getNonCanaryIngress(sources)
-			if ingress == nil {
-				continue
-			}
-
-			val, ok := ingress.Annotations[UseRegexAnnotation]
-			if !ok {
-				continue
-			}
-
-			useRegex, err := strconv.ParseBool(val)
-			if err != nil {
-				// Invalid boolean value, default to false or log invalid usage?
-				// For now, let's treat invalid as false (safe default).
-				// We definitely shouldn't panic or crash.
-				continue
-			}
-
-			if useRegex {
-				for matchIdx := range rule.Matches {
-					path := rule.Matches[matchIdx].Path
-					if path != nil {
-						val := gatewayv1.PathMatchRegularExpression
-						path.Type = &val
-					}
+		for i, rule := range httpRouteCtx.Spec.Rules {
+			for j, path := range rule.Matches {
+				if path.Path != nil  {
+					// Ingress nginx regex path matches are prefix matches by default
+					httpRouteCtx.Spec.Rules[i].Matches[j].Path.Type = ptr.To[gatewayv1.PathMatchType](gatewayv1.PathMatchRegularExpression)
+					httpRouteCtx.Spec.Rules[i].Matches[j].Path.Value = ptr.To(*httpRouteCtx.Spec.Rules[i].Matches[j].Path.Value + ".*")
+					// TODO. They are also case insensitive.
 				}
 			}
 		}
